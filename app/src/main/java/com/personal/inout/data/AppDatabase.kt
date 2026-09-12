0f05993784b8617f6506f7dffc423466ea564a13
snj147 <snj147@gmail.com>
2026-09-13 04:54:06 +0530

    Create AppDatabase.kt

---
+++ app/src/main/java/com/personal/inout/data/AppDatabase.kt
@@ -0,0 +1,38 @@
+package com.personal.inout.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+
+@Database(entities = [Account::class, Transaction::class, Debt::class, SmsDraft::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun vaultDao(): VaultDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                // Pre-stretching your 4-digit master PIN using SQLCipher passphrase
+                val passphrase = SQLiteDatabase.getBytes("1234".toCharArray())
+                val factory = SupportFactory(passphrase)
+
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "inout_encrypted.db"
+                )
+                .openHelperFactory(factory)
+                .fallbackToDestructiveMigration()
+                .build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
