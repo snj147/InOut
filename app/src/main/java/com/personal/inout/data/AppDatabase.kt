203652453196fc2af89f1a43114fada8dbdb28c9
snj147 <snj147@gmail.com>
2026-09-15 07:48:35 +0530

    Update AppDatabase.kt

--- app/src/main/java/com/personal/inout/data/AppDatabase.kt
+++ app/src/main/java/com/personal/inout/data/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
 
-@Database(entities = [Account::class, Transaction::class, Counterparty::class, SmsDraft::class], version = 4, exportSchema = false)
+@Database(entities = [Account::class, Transaction::class, Counterparty::class, SmsDraft::class], version = 6, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun vaultDao(): VaultDao
 
