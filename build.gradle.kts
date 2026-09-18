8d0dac92fb5780a89cc0737086713acd243f1198
snj147 <snj147@gmail.com>
2026-09-18 07:42:30 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    kotlin("kapt")
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
+
 
 android {
     namespace = "com.personal.inout"
